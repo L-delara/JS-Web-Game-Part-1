f4022bae5af2b559f73e7b85762036eb11c0a7cc
Ldelara <ligia.delara15@gmail.com>
2023-03-07 20:02:16 -0500

    refactor into function to add images

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-let greenCharacter = document.createElement("img");
-greenCharacter.src = "assets/green-character.gif";
-greenCharacter.style.position = "fixed";
-greenCharacter.style.left = "100px";
-greenCharacter.style.bottom = "100px";
-document.body.append(greenCharacter);
+function newImage(url, left, bottom) {
+  let object = document.createElement("img");
+  object.src = url;
+  object.style.position = "fixed";
+  object.style.left = left + "px";
+  object.style.bottom = bottom + "px";
+  document.body.append(object);
+  return object;
+}
 
-let pineTree = document.createElement("img");
-pineTree.src = "assets/pine-tree.png";
-pineTree.style.position = "fixed";
-pineTree.style.left = "450px";
-pineTree.style.bottom = "200px";
-document.body.append(pineTree);
+newImage("/assets/green-character.gif", 200, 200);
+newImage("/assets/red-character.gif", 200, 400);
+newImage("/assets/pine-tree.png", 600, 300);
+newImage("/assets/sword.png", 300, 400);
+newImage("/assets/sheild.png", 300, 300);
+newImage("/assets/staff.png", 300, 200);
